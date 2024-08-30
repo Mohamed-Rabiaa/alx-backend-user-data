dbaf37d8c84f9f922b7cab50166d1fd7541272d3
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-31 01:29:47 +0300

    Add main function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,6 +12,7 @@ from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
+
 def filter_datum(
         fields: List[str],
         redaction: str,
@@ -25,6 +26,7 @@ def filter_datum(
     return re.sub(pattern, lambda m: "{}={}".format(
         m.group().split('=')[0], redaction), message)
 
+
 def get_logger() -> logging.Logger:
     """
     get_logger
@@ -32,13 +34,14 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    
+
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
 
     logger.addHandler(handler)
     return logger
+
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
@@ -50,8 +53,37 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     database = os.getenv('PERSONAL_DATA_DB_NAME', '')
 
     return mysql.connector.connect(host=host, user=user, port=3306,
-                                password=password, database=database)
-    
+                                   password=password, database=database)
+
+
+def main() -> None:
+    """
+    main
+    """
+    try:
+        db = get_db()
+        with db.cursor() as cursor:
+            cursor.execute('SELECT * FROM users')
+            result = cursor.fetchall()
+            logger = get_logger()
+
+            for row in result:
+                name, email, phone, ssn, password, ip, \
+                    last_login, user_agent = row
+                message = (
+                    'name={}; email={}; phone={}; ssn={}; password={}; '
+                    'ip={}; last_login={}; user_agent={};').format(
+                    name, email, phone, ssn, password, ip, last_login, user_agent)
+                logger.info(message)
+
+    except mysql.connector.Error as err:
+        logging.error("Database error: {}".format(err))
+    except Exception as err:
+        logging.error("Unexpected error: {}".format(err))
+    finally:
+        db.close()
+
+
 class RedactingFormatter(logging.Formatter):
     """
     Redacting Formatter class
@@ -72,3 +104,7 @@ class RedactingFormatter(logging.Formatter):
         message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
+
+
+if __name__ == '__main__':
+    main()
