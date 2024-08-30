611be0ed413874a3c50cf4574a89ceab694a8f0c
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-30 21:18:55 +0300

    Fix code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,28 +11,6 @@ import logging
 PII_FIELDS = ("email", "phone", "ssn", "password", "ip")
 
 
-class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """
-        format
-        """
-        message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                            message, self.SEPARATOR)
-
-
 def filter_datum(
         fields: List[str],
         redaction: str,
@@ -56,7 +34,29 @@ def get_logger() -> logging.Logger:
 
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
-    handle.setFormatter(RedactingFormatter())
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
 
     logger.addHandler(handler)
     return logger
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        format
+        """
+        message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
