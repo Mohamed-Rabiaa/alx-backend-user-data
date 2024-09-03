10f927aaa8f41c03f747b7c11cb72bcf1b3af204
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-09-03 07:34:02 +0300

    Update require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,11 +11,20 @@ class Auth():
     """
     Manages the API authentication
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         require_auth
         """
-        return False
+        if isinstance(path, str) and path.endswith('/') is False:
+            path = path + '/'
+
+        if path is None or excluded_paths is None \
+           or len(excluded_paths) == 0 or path not in excluded_paths:
+            return True
+
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """
