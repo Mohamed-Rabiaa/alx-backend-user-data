051e8519ae4d675b21f991341444e3d4fad9fb13
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-09-03 11:32:37 +0300

    Add extract_base64_authorization_header function

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,20 +21,25 @@ if os.getenv('AUTH_TYPE') == 'auth':
 elif os.getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-    
+
+
 @app.before_request
 def pre_request():
     """
     pre_request
     """
-    excluded_list = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
     if auth is None or auth.require_auth(request.path, excluded_list) is False:
         return
     if auth.authorization_header(request) is None:
-       abort(401)
+        abort(401)
     if auth.current_user(request) is None:
         abort(403)
-                          
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,18 @@ class BasicAuth(Auth):
     """
     BasicAuth
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header is None \
+           or not isinstance(authorization_header, str) \
+           or authorization_header.startswith('Basic ') is False:
+            return None
+        else:
+            base64_part = authorization_header.split()[1]
+            return base64_part
