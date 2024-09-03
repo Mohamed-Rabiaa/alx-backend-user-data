8d9de0be8c1337ddf9181026a7b2a4a8c36f296a
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-09-04 01:29:37 +0300

    Add user_object_from_credentials function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,9 @@ This module contains the BasicAuth class
 """
 
 from api.v1.auth.auth import Auth
+from models.user import User
 from base64 import b64decode
+from typing import TypeVar
 import binascii
 
 
@@ -59,3 +61,22 @@ class BasicAuth(Auth):
         user_name = credentials[0]
         password = credentials[1]
         return (user_name, password)
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on his email and password
+        """
+        if user_email is None or not isinstance(user_email, str) \
+           or user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users_list = User.search({'email': user_email})
+        if len(users_list) == 0:
+            return None
+        user = users_list[0]
+        if user.is_valid_password(user_pwd) is False:
+            return None
+        return user
