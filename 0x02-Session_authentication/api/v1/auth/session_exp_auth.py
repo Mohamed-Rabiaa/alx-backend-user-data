fd9563a08f2eb6c3e04ba720bbb5b4b6576bc469
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-09-05 10:25:50 +0300

    Add SessionExpAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+This module contains the SessionExpAuth class
+"""
+
+from api.v1.auth.session_auth import SessionAuth
+import os
+from datetime import datetime, timedelta
+
+
+class SessionExpAuth(SessionAuth):
+    """
+    SessionExpAuth
+    """
+
+    def __init__(self):
+        try:
+            self.session_duration = int(os.getenv('SESSION_DURATION', 0))
+        except ValueError:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """
+        Overloads create_session
+        """
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dictionary = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+        Overloads user_id_for_session_id
+        """
+        if session_id is None:
+            return None
+        user_id = self.user_id_by_session_id.get(session_id)
+        if user_id is None:
+            return None
+
+        if self.session_duration <= 0:
+            return user_id
+
+        session_dictionary = self.user_id_by_session_id.get(session_id)
+        created_at = session_dictionary.get('created_at')
+        if created_at is None:
+            return None
+
+        time_delta = timedelta(seconds=self.session_duration)
+        expiration_time = created_at + time_delta
+        if expiration_time < datetime.now():
+            return None
+        return session_dictionary['user_id']
