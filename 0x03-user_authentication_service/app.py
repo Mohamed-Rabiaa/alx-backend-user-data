b5f44e3dc533d4327ddc7922b0b3d70bcd1ba6e1
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-09-10 20:19:31 +0300

    Add get_reset_password_token function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,6 +75,18 @@ def profile() -> str:
         abort(403)
     return jsonify({"email": user.email}), 200
 
+@app.route('/reset_password', methods=['POST'],
+           strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    Handles /reset_password route
+    """
+    email = request.form.get('email')
+    if email is None:
+        abort(403)
+    reset_token = AUTH.get_reset_password_token(email)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
